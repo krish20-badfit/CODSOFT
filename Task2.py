4e29a07af91a1ea8d3dbc691b2ff21c9933ca2d2
krish20-badfit <krishnagirik01@gmail.com>
2025-07-04 11:39:25 +0530

    Add Task2.py

--- Task2.py
+++ Task2.py
@@ -88,4 +88,113 @@ def numerator(t):
 
 
 
-def click(event)
+def click(event):
+    global exp
+    global f 
+
+    t = event.widget.cget("text")
+    if screenvar.get()!="ERROR PRESS C":
+
+        if t=="C":
+            clear()
+
+        elif t=="%":
+            screenvar.set(screenvar.get()+t)
+            exp+="/100"
+
+        elif t=="S<>D":
+            frac()
+        elif t=="=":
+            calculate()
+        else:
+            numoperator(t)
+    else:
+        if t=="C":
+            clear():
+
+
+f1 = tkinter.Frame(r,bg="black")
+
+img = tkinter.PhotoImage(file='image.png')
+img2=img.subsample(5)
+heading=tkinter.Label(f1,image=img2,bg="black")
+heading.pack(fill="x")
+
+screenvar=tkinter.StringVar()
+screenentry=tkinter.Entry(f1,width=30,text=screenvar.fg="black",bg="#e5f2e5",font=("Eurostile",15,"bold"),justify="right")
+screenentry.pack(fill="x")
+
+
+l=tkinter.Label(f1,bg="black")
+l.pack()
+f1.pack(fill="x")
+
+f2=tkinter.Frame(r,bg="#3b3b3b")
+button=tkinter.Button(f2,text="OFF",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"),command=quit)
+button.grid(row=0,column=0,padx=8,pady=8)
+button.tkinter.Button(f2,text="S<>D",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=0,column=1,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="%",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=0,column=2,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="+",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=0,column=3,padx=8,pady=8)
+button.bind("<Button-1>",click)
+
+button=tkinter.Button(f2,text="7",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=1,column=0,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button.tkinter.Button(f2,text="8",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=1,column=1,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="9",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=1,column=2,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="x",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=1,column=3,padx=8,pady=8)
+button.bind("<Button-1>",click)
+
+button=tkinter.Button(f2,text="4",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=2,column=0,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="5",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=2,column=1,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="6",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=2,column=2,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="-",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=2,column=3,padx=8,pady=8)
+button.bind("<Button-1>",click)
+
+button=tkinter.Button(f2,text="1",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=3,column=0,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="2",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=3,column=1,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="3",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=3,column=2,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="+",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=3,column=3,padx=8,pady=8)
+button.bind("<Button-1>",click)
+
+button=tkinter.Button(f2,text="C",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=4,column=0,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="0",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=4,column=1,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text=".",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=4,column=2,padx=8,pady=8)
+button.bind("<Button-1>",click)
+button=tkinter.Button(f2,text="=",height=1,width=4,fg="white",bg="black",font=("Arial",10,"bold"))
+button.grid(row=4,column=3,padx=8,pady=8)
+button.bind("<Button-1>",click)
+f2.pack(pady=15)
+
+
+
+r.mainloop()
